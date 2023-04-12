e9fe02945b4d579c8b50a7d9ed7988f151e2a607
Bala Sukesh <bbelide2@illinois.edu>
2023-04-12 09:46:26 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ url = "https://6rsqbadgd5jm4jj52ezggecgqu0dbnol.lambda-url.us-east-1.on.aws/"
 payload = {
             "submitterEmail": ' ', # Your Email Id as it appears in the coursera instruction page.
             "secret": ' ', # Your token as it appears in the coursera instruction page. This token will only be valid for 30 mins.
-            "ipaddress": '<IPv4>:<Port>' # Public IPv4 address which you can find on te EC2 instance home page. Add port number on which your server is running.
+            "ipaddress": '<IPv4>:<Port>' # Public IPv4 address which you can find on the EC2 instance home page. Add port number on which your server is running.
     }
 
 
