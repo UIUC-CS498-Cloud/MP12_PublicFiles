466401fe64e43be24d2eedb5722a11a5edfaa9c1
Bala Sukesh <bbelide2@illinois.edu>
2023-05-13 20:09:20 -0500

    Update test.py with new autograder's url
    Updated test.py file with the url of the new autograder.

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import requests
 import json
 
-url = "https://6rsqbadgd5jm4jj52ezggecgqu0dbnol.lambda-url.us-east-1.on.aws/"
+url = "https://vh4xhmjk3lw2ibz5k6sa4i6fym0ltkqv.lambda-url.us-east-1.on.aws/"
 
 payload = {
             "submitterEmail": ' ', # Your Email Id as it appears in the coursera instruction page.
             "secret": ' ', # Your token as it appears in the coursera instruction page. This token will only be valid for 30 mins.
-            "ipaddress": '<IPv4>:<Port>' # Public IPv4 address which you can find on the EC2 instance home page. Add port number on which your server is running.
+            "ipaddress": '<IPv4>:<Port>' # Public IPv4 address which you can find on the EC2 instance home page. Add port number on which your server is running (5000).
     }
 
 
